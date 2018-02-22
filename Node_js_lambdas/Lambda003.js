50f422bc7a0a66cda58e4c31a714c4bd6381628f
david noakes <david.noakes@gmail.com>
2018-02-22 14:38:36 +1000

    example from AWS course

---
+++ Node_js_lambdas/Lambda003.js
@@ -0,0 +1,15 @@
+'use strict'
+/*
+* Lambda function to process a dynamoDB stream from a table
+*/
+console.log('Loading function');
+
+exports.handler = (event, context, callback) => {
+	//console.log('Received event:', JSON.stringify(event, null, 2));
+	event.Records.forEach((record) => {
+		console.log(record.eventID);
+		console.log(record.eventName);
+		console.log('DynamoDB Record: %j', record.dynamoDB);
+	});
+	callback(null, 'Successfully processed ${event.Records.length} records.');
+}
